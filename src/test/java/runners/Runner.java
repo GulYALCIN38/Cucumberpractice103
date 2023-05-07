09099906d77be6cfdef56604fa0c05f7b3b828ad
GulYALCIN38 <117485421+GulYALCIN38@users.noreply.github.com>
2023-05-07 23:30:56 +0200

    Update Runner.java

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -7,8 +7,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)//test calistirici notasyon
 @CucumberOptions(//senaryolarin nerede nasil calisacagi ve hangi rapru kullanacagini ayarlar
-plugin = {"html:target/cucumberReport.html",
-         "json:target/json-reports/cucumber.json"},
+plugin = {"html:src/test/resources/reports/cucumberReport.html",
+         "json:src/test/resources/reports/cucumber.json"},
 
         features="src/test/resources/features",//features dosya yolunu yazariz
         glue="stepdefinitions",//kodlarin yazildigi yeri belirtiriz
